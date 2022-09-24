4e7c5a760a592a83f5985e39a79a1a1b12db6ca0
El-Khawaga <rocky@gmail.com>
2022-09-25 01:41:43 +0200

    First Commit

---
+++ Js/Main.js
@@ -0,0 +1,13 @@
+
+function initMap() {
+    const location = { lat: -25.344, lng: 131.031 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location,
+    });
+
+}
+
+
+
+
